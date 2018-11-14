8d6b08faac788d05670d6800efb30d37c6c3ec80
Natasha S Love <debora@inbox.ru>
2018-11-13 19:27:39 -0600

    main update

--- app/src/main/java/com/example/debor/simpletodo/MainActivity.java
+++ app/src/main/java/com/example/debor/simpletodo/MainActivity.java
@@ -2,12 +2,43 @@ package com.example.debor.simpletodo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    DbHelper dbHelper;
+    ArrayAdapter<String> mAdapter;
+    ListView lstTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dbHelper = new DbHelper(this);
+        lstTask = (ListView)findViewById(R.id.lstTask);
+        loadTaskList();
+
     }
+
+    private void loadTaskList(){
+
+        ArrayList<String> taskList = dbHelper.getTaskList();
+        if (mAdapter==null){
+
+            mAdapter= new ArrayAdapter<String>(this, R.layout.row, R.id.task_title, taskList);
+            lstTask.setAdapter(mAdapter);
+        }
+
+        else {
+
+            mAdapter.clear();
+            mAdapter.addAll(taskList);
+            mAdapter.notifyDataSetChanged();
+        }
+    }
+
 }
